0ee7aa2cfc7ee65db995112e30a750eeaaf831f4
Trevor Hammond <trevorhammond@gmail.com>
2017-11-03 06:12:37 -0400

    little bit of cleanup

--- TableSaltCli.go
+++ TableSaltCli.go
@@ -13,6 +13,7 @@ import (
     "golang.org/x/crypto/ssh/agent"
 )
 
+var saltCommand string
 var configuration = Configuration{}
 var bsshClientConnection *ssh.Client = nil
 var sshConfig *ssh.ClientConfig = nil
@@ -24,7 +25,6 @@ type Configuration struct {
     JumpPassword string
     JumpPrivateKey string
     JumpServer string
-    LocalEndpoint string
     RemoteEndpoint string
     RemoteUsername string
     RemotePassword string
@@ -36,6 +36,89 @@ func SSHAgent() ssh.AuthMethod {
         return ssh.PublicKeysCallback(agent.NewClient(sshAgent).Signers)
     }
     return nil
+}
+
+func setupJump() {
+
+    err := error(nil)
+    bsshAuthMethod := []ssh.AuthMethod{SSHAgent()}
+
+    if configuration.Auth == "key" && len(configuration.JumpPrivateKey) > 0 {
+        jumpKey, err := ssh.ParsePrivateKey([]byte(configuration.JumpPrivateKey))
+        if err != nil {
+            fmt.Println("Could not parse private key file. Check the path and ensure it is not encrypted.")
+            os.Exit(1)
+        }
+        bsshAuthMethod[0] = ssh.PublicKeys(jumpKey)
+    } else if configuration.Auth == "agent" && runtime.GOOS != "windows" {
+        bsshAuthMethod[0] = SSHAgent()
+    } else if configuration.Auth == "password" && len(configuration.JumpPassword) > 0 {
+        bsshAuthMethod[0] = ssh.Password(configuration.JumpPassword)
+    } else {
+        fmt.Println("No supported authentication modes available/supported. Double check your configuration.")
+        os.Exit(1)
+    }
+
+    bsshConfig := &ssh.ClientConfig{
+        User: configuration.JumpUsername,
+        Auth: bsshAuthMethod,
+        HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+    }
+
+    bsshClientConnection, err = ssh.Dial("tcp", configuration.JumpServer, bsshConfig)
+    if err != nil {
+        log.Fatal(err)
+        os.Exit(1)
+    }
+
+}
+
+func useJump() (string) {
+
+    jumpConnection, err := bsshClientConnection.Dial("tcp", configuration.RemoteEndpoint)
+    if err != nil {
+        log.Fatal(err)
+        os.Exit(1)
+    }
+
+    ncc, chans, reqs, err := ssh.NewClientConn(jumpConnection, configuration.RemoteEndpoint, sshConfig)
+    if err != nil {
+        log.Fatal(err)
+        os.Exit(1)
+    }
+
+    sshClientConnection := ssh.NewClient(ncc, chans, reqs)
+
+    session, err := sshClientConnection.NewSession()
+    if err != nil {
+        fmt.Println(err.Error())
+    }
+    defer session.Close()
+    var b bytes.Buffer
+    session.Stdout = &b
+    err = session.Run(saltCommand)
+    return b.String()
+
+}
+
+func goDirect() (string) {
+
+    sshClientConnection, err := ssh.Dial("tcp", configuration.RemoteEndpoint, sshConfig)
+    if err != nil {
+        log.Fatal(err)
+        os.Exit(1)
+    }
+
+    session, err := sshClientConnection.NewSession()
+    if err != nil {
+        fmt.Println(err.Error())
+    }
+    defer session.Close()
+    var b bytes.Buffer
+    session.Stdout = &b
+    err = session.Run(saltCommand)
+    return b.String()
+
 }
 
 func main() {
@@ -57,41 +140,11 @@ func main() {
     }
 
     // Format salt command
-    saltCommand := "salt " + strings.Join(args, " ")
+    saltCommand = "salt " + strings.Join(args, " ")
 
-    // Use bastion/jump server?
+    // Connect to bastion/jump server if necessary
     if configuration.UseJump {
-
-        bsshAuthMethod := []ssh.AuthMethod{SSHAgent()}
-
-        if configuration.Auth == "key" && len(configuration.JumpPrivateKey) > 0 {
-            jumpKey, err := ssh.ParsePrivateKey([]byte(configuration.JumpPrivateKey))
-            if err != nil {
-                fmt.Println("Could not parse private key file. Check the path and ensure it is not encrypted.")
-                os.Exit(1)
-            }
-            bsshAuthMethod[0] = ssh.PublicKeys(jumpKey)
-        } else if configuration.Auth == "agent" && runtime.GOOS != "windows" {
-            bsshAuthMethod[0] = SSHAgent()
-        } else if configuration.Auth == "password" && len(configuration.JumpPassword) > 0 {
-            bsshAuthMethod[0] = ssh.Password(configuration.JumpPassword)
-        } else {
-            fmt.Println("No supported authentication modes available/supported. Double check your configuration.")
-            os.Exit(1)
-        }
-
-        bsshConfig := &ssh.ClientConfig{
-            User: configuration.JumpUsername,
-            Auth: bsshAuthMethod,
-            HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-        }
-
-        bsshClientConnection, err = ssh.Dial("tcp", configuration.JumpServer, bsshConfig)
-        if err != nil {
-            log.Fatal(err)
-            os.Exit(1)
-        }
-
+        setupJump()
     }
 
     sshAuthMethod := []ssh.AuthMethod{SSHAgent()}
@@ -118,52 +171,15 @@ func main() {
         HostKeyCallback: ssh.InsecureIgnoreHostKey(),
     }
 
+    // Execute salt command
+    var saltOutput string
     if configuration.UseJump {
-
-        jumpConnection, err := bsshClientConnection.Dial("tcp", configuration.RemoteEndpoint)
-        if err != nil {
-            log.Fatal(err)
-            os.Exit(1)
-        }
-
-        ncc, chans, reqs, err := ssh.NewClientConn(jumpConnection, configuration.RemoteEndpoint, sshConfig)
-        if err != nil {
-            log.Fatal(err)
-            os.Exit(1)
-        }
-
-        sshClientConnection := ssh.NewClient(ncc, chans, reqs)
-
-        session, err := sshClientConnection.NewSession()
-        if err != nil {
-            fmt.Println(err.Error())
-        }
-        defer session.Close()
-        var b bytes.Buffer
-        session.Stdout = &b
-        err = session.Run(saltCommand)
-        fmt.Println(b.String())
-
-
+        saltOutput = useJump()
+    // Go directly to salt-master
     } else {
-
-        sshClientConnection, err := ssh.Dial("tcp", configuration.RemoteEndpoint, sshConfig)
-        if err != nil {
-            log.Fatal(err)
-            os.Exit(1)
-        }
-
-        session, err := sshClientConnection.NewSession()
-        if err != nil {
-            fmt.Println(err.Error())
-        }
-        defer session.Close()
-        var b bytes.Buffer
-        session.Stdout = &b
-        err = session.Run(saltCommand)
-        fmt.Println(b.String())
-
+        saltOutput = goDirect()
     }
 
+    fmt.Println(saltOutput)
 
 }
